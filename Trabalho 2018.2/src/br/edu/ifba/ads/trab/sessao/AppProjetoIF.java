b17505b847b9321e3af51fc49267a332220236ca
Victor Souza <45768977+Mushidon@users.noreply.github.com>
2019-04-23 16:52:52 -0300

    Acrescentando metodos na interface

--- Trabalho 2018.2/src/br/edu/ifba/ads/trab/sessao/AppProjetoIF.java
+++ Trabalho 2018.2/src/br/edu/ifba/ads/trab/sessao/AppProjetoIF.java
@@ -1,10 +1,16 @@
 package br.edu.ifba.ads.trab.sessao;
 
 
+import java.sql.SQLException;
+
 import br.edu.ifba.ads.trab.entidades.Coordenadas;
 import br.edu.ifba.ads.trab.entidades.VeiculoAutonomo;
 
 public interface AppProjetoIF {
 	public VeiculoAutonomo calculateMenorCusto(Coordenadas pontoDest, double peso) throws Exception;
 	public void atualizarCoordenadas (Coordenadas pontoDest, String matricula) throws Exception;
+	public double getMenorCusto (Coordenadas pontoDest, double peso) throws SQLException, Exception;
+	public String getMatriculaMenor (Coordenadas pontoDest, double peso) throws SQLException, Exception;
+	    	
+	
 }
